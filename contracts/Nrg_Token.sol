d45f6d86d17731fd4d7b425f35312f05f23323ab
heildever <a.3991@live.com>
2018-08-25 19:04:02 -0400

    Crowdfunding mechanism with NFT
    initial commit

---
+++ contracts/Nrg_Token.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Nrg_Token is ERC721Token, Ownable {
+
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Nrg_Token = artifacts.require("Nrg_Token");
+
+module.exports = function(deployer) {
+  deployer.deploy(Nrg_Token);
+};
