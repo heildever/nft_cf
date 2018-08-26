a94f02bb6f3e06f0739e1cc49d13f143a5252c74
heildever <a.3991@live.com>
2018-08-26 10:26:00 -0400

    receiveEther()

--- contracts/Nrj_Token.sol
+++ contracts/Nrj_Token.sol
@@ -1,14 +1,23 @@
 pragma solidity ^0.4.24;
+//consider locking pragma
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Nrj_Token is ERC721Token, Ownable {
+    string public name = "Enerjee";
+    string public symbol = "NRJ";
 
     constructor(string _name, string _symbol) ERC721Token(_name, _symbol) public {
-        _name = "Enerjee";
-        _symbol_ = "NRJ";
+        name = _name;
+        symbol = _symbol;
     }
 
+    function receiveEther(uint256 amount) payable public {
+        require(msg.value == amount);
+    }
 }
+/* cap should be defined to limit received erc20s
+then limit will be divided into # sender address
+*/
