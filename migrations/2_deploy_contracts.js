41679e2aaac86b7c01f9a0aa2c17d8ac595f5df9
heildever <a.3991@live.com>
2018-08-28 09:27:57 -0400

    Deployer needed to be changed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Nrj_Token = artifacts.require("Nrj_Token");
 
 module.exports = function(deployer) {
-  deployer.deploy(Nrj_Token);
+  var name = 'Enerjee';
+  var symbol = 'RNJ';
+  deployer.deploy(Nrj_Token, name, symbol);
 };
